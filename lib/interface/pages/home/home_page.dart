1d16a11f4fc54868ac5becb3c8f1b5a06d1fcb0d
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-11 18:10:12 -0300

    Adding the Card content text.

--- lib/interface/pages/home/home_page.dart
+++ lib/interface/pages/home/home_page.dart
@@ -47,6 +47,17 @@ class HomePage extends StatelessWidget {
                     aspectRatio: 18.0 / 11.0,
                     child: Image.asset('lib/interface/assets/diamond.png'),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary Text'),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
