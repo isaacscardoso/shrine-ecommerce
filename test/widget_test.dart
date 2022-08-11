abe882cee10aa4bb02f8e94a19666441488fae60
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-11 17:19:13 -0300

    solving bug

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:shrine_ecommerce/main.dart';
+import 'package:shrine_ecommerce/shrine_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ShrineApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
