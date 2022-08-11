85a81608aa67e19f1f1344c4c851557f92e9fef2
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-11 17:24:08 -0300

    Adding the 'Menu' button in the AppBar.

--- lib/interface/pages/home/home_page.dart
+++ lib/interface/pages/home/home_page.dart
@@ -7,6 +7,13 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () => print('Menu Button'),
+        ),
         title: const Text('SHRINE'),
       ),
       body: const Center(
