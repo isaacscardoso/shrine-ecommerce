78cf05922f9499b4a400401de1360d8b54cf0f5a
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-11 17:22:09 -0300

    Adding the AppBar on homepage.

--- lib/interface/pages/home/home_page.dart
+++ lib/interface/pages/home/home_page.dart
@@ -5,8 +5,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SHRINE'),
+      ),
+      body: const Center(
         child: Text('You are logged in!'),
       ),
     );
