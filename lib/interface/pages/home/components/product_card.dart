f41e387da2b7f93cceacfa6b49525c0471bafc9e
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-11 22:13:02 -0300

    Only the re-implementation of the Card.

--- lib/interface/pages/home/components/product_card.dart
+++ lib/interface/pages/home/components/product_card.dart
@@ -5,6 +5,28 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Card Product works!');
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 18.0 / 11.0,
+            child: Image.asset('lib/interface/assets/diamond.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                Text('Title'),
+                SizedBox(height: 8.0),
+                Text('Secondary Text'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
