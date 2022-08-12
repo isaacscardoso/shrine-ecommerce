910aab587d6fa429daf58afd6049bb8925299e0a
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-11 21:39:58 -0300

    Initial implementation of the GridProductCard component.

---
+++ lib/interface/pages/home/components/grid_product_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GridProductCard extends StatelessWidget {
+  const GridProductCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Grid Card Product works!');
+  }
+}
