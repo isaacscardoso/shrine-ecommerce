b96acd72c12ea9cc97d9bf9faaf7ed1125c9335b
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-12 00:21:11 -0300

    Refactoring of GridProductCard.

--- lib/interface/pages/home/components/grid_product_card.dart
+++ lib/interface/pages/home/components/grid_product_card.dart
@@ -1,8 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:shrine_ecommerce/interface/pages/home/components/product_card.dart';
+import 'package:intl/intl.dart';
+import 'package:shrine_ecommerce/data/products_repository.dart';
+import 'package:shrine_ecommerce/model/category.dart';
+import 'package:shrine_ecommerce/model/product.dart';
 
 class GridProductCard extends StatelessWidget {
   const GridProductCard({Key? key}) : super(key: key);
+
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData themeData = Theme.of(context);
+    final NumberFormat numberFormat = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString(),
+    );
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset('lib/interface/assets/diamond.png'),
+              // child: Image.asset(
+              //   product.assetName,
+              //   package: product.assetPackage,
+              //   fit: BoxFit.fitWidth,
+              // ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.name,
+                      style: themeData.textTheme.headline6,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      numberFormat.format(product.price),
+                      style: themeData.textTheme.subtitle2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +66,7 @@ class GridProductCard extends StatelessWidget {
       crossAxisCount: 2,
       padding: const EdgeInsets.all(16.0),
       childAspectRatio: 8.0 / 9.0,
-      children: const <Widget>[
-        ProductCard(),
-      ],
+      children: _buildGridCards(context),
     );
   }
 }
