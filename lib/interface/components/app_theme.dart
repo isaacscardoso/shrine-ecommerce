6137505e63a87e1471a6461bc8dd3a970959c0a3
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-13 16:44:43 -0300

    Implementing custom text theme.

--- lib/interface/components/app_theme.dart
+++ lib/interface/components/app_theme.dart
@@ -2,13 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:shrine_ecommerce/interface/colors/theme_color_palette.dart';
 
 ThemeData buildShrineTheme() {
-  final ThemeData themeData = ThemeData.light();
-  return themeData.copyWith(
-    colorScheme: themeData.colorScheme.copyWith(
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
       primary: kShrinePink100,
       onPrimary: kShrineBrown900,
       secondary: kShrineBrown900,
       error: kShrineErrorRed,
     ),
+    textTheme: buildShrineTextTheme(base.textTheme),
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: kShrinePink100,
+    ),
   );
+}
+
+TextTheme buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headline5: base.headline5!.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        headline6: base.headline6!.copyWith(
+          fontSize: 18.0,
+        ),
+        caption: base.caption!.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+        bodyText1: base.bodyText1!.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 16.0,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: kShrineBrown900,
+        bodyColor: kShrineBrown900,
+      );
 }
