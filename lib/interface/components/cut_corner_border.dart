9fba94a8cd2dbbf700ccbcc224ca96f48975353b
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-16 00:05:16 -0300

    Implementation of the 'copyWith' method of the CutCornerBorder component.

--- lib/interface/components/cut_corner_border.dart
+++ lib/interface/components/cut_corner_border.dart
@@ -13,4 +13,32 @@ class CutCornerBorder extends OutlineInputBorder {
           borderRadius: borderRadius,
           gapPadding: gapPadding,
         );
+
+  @override
+  CutCornerBorder copyWith({
+    BorderSide? borderSide,
+    BorderRadius? borderRadius,
+    double? gapPadding,
+    double? cut,
+  }) {
+    return CutCornerBorder(
+      borderRadius: borderRadius ?? this.borderRadius,
+      borderSide: borderSide ?? this.borderSide,
+      gapPadding: gapPadding ?? this.gapPadding,
+      cut: cut ?? this.cut,
+    );
+  }
+
+  @override
+  ShapeBorder? lerpFrom(ShapeBorder? x, double y) {
+    if (x is CutCornerBorder) {
+      final CutCornerBorder outline = x;
+      return CutCornerBorder(
+        borderRadius: BorderRadius.lerp(outline.borderRadius, borderRadius, y)!,
+        borderSide: BorderSide.lerp(outline.borderSide, borderSide, y),
+        gapPadding: outline.gapPadding,
+        cut: cut,
+      );
+    }
+  }
 }
